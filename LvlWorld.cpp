d8afa11b23b9acdcb087c226ca1ae4453232e186
BlackBeetleHub <kyrylo.afendi@nure.ua>
2016-09-30 22:57:47 +0300

    Scrolling is done.

--- LvlWorld.cpp
+++ LvlWorld.cpp
@@ -15,8 +15,8 @@ void LvlWorld::scrool(int x, int y) {
 void LvlWorld::draw(sf::RenderWindow & window) {
 	for (int i = 0; i < terra.size(); i++) {
 		b2Vec2 pos = cam->GetPosition();
-		terra[i].sprite.setPosition(terra[i]._x - pos.x + 683 - terra[i]._width/2, terra[i]._y - pos.y - terra[i]._hight/2 + 384);
-		window.draw(terra[i].sprite); 
+		terra[i].sprite.setPosition(terra[i]._x - pos.x + 683 + 20, terra[i]._y - pos.y  + 384 + 30);
+		window.draw(terra[i].sprite);
 	}
 }
 
